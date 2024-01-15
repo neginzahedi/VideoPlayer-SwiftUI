59504b7f43aaadd7dcb3021a34dad121dbbeb779
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-15 17:52:33 -0500

    Initial Commit

---
+++ VideoPlayerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Michael Gauthier on 2021-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoPlayerSwiftUI/VideoPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerSwiftUIApp.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Michael Gauthier on 2021-01-06.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
