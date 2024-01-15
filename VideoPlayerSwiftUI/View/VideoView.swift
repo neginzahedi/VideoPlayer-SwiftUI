42873b5c5ff053ef020588e1822471c46ce03429
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-15 18:48:34 -0500

    feat: Implement video player view with custom controls

--- VideoPlayerSwiftUI/View/VideoView.swift
+++ VideoPlayerSwiftUI/View/VideoView.swift
@@ -11,18 +11,83 @@ import AVKit
 struct VideoView: View {
     @ObservedObject private var manager = VideoManager.shared
     
+    @State private var player = AVPlayer()
+    @State private var index = 0
+    @State private var isPlaying = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear(){
-                Task {
-                    do {
-                        try await manager.fetchVideos()
-                    } catch{
-                        print("error fetch")
-                    }
+        VStack{
+            Text("Video Player")
+                .bold()
+                .padding()
+            
+            if !manager.videos.isEmpty {
+                VideoPlayer(player: player)
+                    .disabled(true) // hide dafault controls
+                    .frame(height: 250)
+                    .overlay(playerControls(), alignment: .center)
+            }
+            
+            Spacer()
+        }
+        .onAppear(){
+            Task {
+                do {
+                    try await manager.fetchVideos()
+                    updatePlayer()
+                } catch{
+                    print("error fetch")
                 }
             }
+        }
+    }
+    
+    // Update the player with the current video
+    private func updatePlayer(){
+        let newPlayer = AVPlayerItem(url: manager.videos[index].hlsURL)
+        self.player.replaceCurrentItem(with: newPlayer)
+    }
+    
+    // Custom player controls view
+    func playerControls() -> some View{
+        HStack(spacing: 80) {
+            // Previous Button
+            Button(action: {
+                if index - 1 >= 0 {
+                    index -= 1
+                    updatePlayer()
+                }
+            }) {
+                Image("previous")
+                    .opacity(index == 0 ? 0.2 : 1.0)
+            }
+            .disabled(index == 0)
+            
+            
+            // Play/Pause Button
+            Button(action: {
+                if isPlaying {
+                    player.pause()
+                } else {
+                    player.play()
+                }
+                isPlaying.toggle()
+            }) {
+                Image(isPlaying ? "pause" : "play")
+            }
+            
+            // Next Button
+            Button(action: {
+                if index + 1 < manager.videos.count {
+                    index += 1
+                    updatePlayer()
+                }
+            }) {
+                Image("next")
+                    .opacity(index + 1 == manager.videos.count ? 0.2 : 1.0)
+            }
+            .disabled(index + 1 == manager.videos.count)
+        }
     }
 }
 
