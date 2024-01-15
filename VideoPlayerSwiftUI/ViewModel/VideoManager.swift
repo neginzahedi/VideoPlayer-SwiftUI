057674eff9f7eaf5cae8c8e2a8bf6138cc9ad7d8
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-15 18:16:51 -0500

    feat: Implement fetchVideos with Swift Concurrency

--- VideoPlayerSwiftUI/ViewModel/VideoManager.swift
+++ VideoPlayerSwiftUI/ViewModel/VideoManager.swift
@@ -16,4 +16,26 @@ class VideoManager: ObservableObject {
     
     private init(){}
     
+    func fetchVideos() async throws{
+        
+        guard let url = URL(string: apiUrl) else {
+            print("invalid url")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print("data recieved")
+            
+            let videos = try JSONDecoder().decode([Video].self, from: data)
+            
+            DispatchQueue.main.async {
+                self.videos = videos.sorted(by: { $0.publishedAt > $1.publishedAt })
+                print("Videos updated and sorted")
+                print(videos)
+            }
+        } catch{
+            print("Failed to fetch videos with error: \(error)")
+        }
+    }
 }
