dae65ab416cf6410127262bebfaa927c667aa40e
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-15 18:07:13 -0500

    refactor: Rename ContentView to VideoView

--- VideoPlayerSwiftUI/View/VideoView.swift
+++ VideoPlayerSwiftUI/View/VideoView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  VideoView.swift
 //  VideoPlayerSwiftUI
 //
 //  Created by Michael Gauthier on 2021-01-06.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct VideoView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        VideoView()
     }
 }
